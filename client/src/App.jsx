17ab6138c0dc8b81f7d871b1b8aee0505af8691c
Polina_teacher <polina_teacher@datalis-mba.local>
2025-08-25 05:27:46 -0700

    I did authorization with localStorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,16 @@ import WelcomePage from './pages/WelcomePage/WelcomePage';
 import HomePage from './pages/HomePage/HomePage';
 import MainLayout from './layouts/MainLayout/MainLayout';
 import './_App.scss'
+import { useState } from 'react';
 
 function App() {
-  const userName = false;
+  const [userName, setUserName] = useState(localStorage.getItem('userName'));
 
   return (
     <Routes>
       <Route
         path="/welcome"
-        element={userName ? <Navigate to="/" replace /> : <WelcomePage />}
+        element={userName ? <Navigate to="/" replace /> : <WelcomePage setUserName={setUserName} />}
       />
 
       <Route
--- client/src/pages/WelcomePage/WelcomePage.jsx
+++ client/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,15 +1,33 @@
 import styles from './_WelcomePage.module.scss';
 import Footer from '../../components/Footer/Footer';
+import { useState } from 'react';
 
-export default function WelcomePage() {
+export default function WelcomePage({ setUserName }) {
+  const [name, setName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (name.trim() && name.length < 30) {
+      localStorage.setItem('userName', name)
+      setUserName(name)
+    }
+  }
+
   return (
     <div className={styles.welcomePage}>
-      <div class="container">
-        <div class={styles.card}>
+      <div className="container">
+        <div className={styles.card}>
           <h2>Добро пожаловать!</h2>
           <p>Введите ваше имя чтобы начать</p>
-          <form>
-            <input type="text" placeholder="Ваше имя" required></input>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Ваше имя"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
             <button type="submit">Продолжить</button>
           </form>
         </div>
